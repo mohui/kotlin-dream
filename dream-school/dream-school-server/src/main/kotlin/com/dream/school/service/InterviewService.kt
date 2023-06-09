d2334417661641d9f48c1d4a76fa490df09c443b
王贺辉 <wanghehui@bjknrt.com>
2023-06-09 15:59:37 +0800

    添加备注

--- dream-school/dream-school-server/src/main/kotlin/com/dream/school/service/InterviewService.kt
+++ dream-school/dream-school-server/src/main/kotlin/com/dream/school/service/InterviewService.kt
@@ -1,10 +1,16 @@
 package com.dream.school.service
 
+/**
+ * 算法service
+ */
 interface InterviewService {
     /**
      * 百元买百鸡
      */
     fun chicken()
 
+    /**
+     * 冒泡排序
+     */
     fun bubbleSort(array: IntArray)
 }
